87c9c9b4f14e874d781ef30a51105e03085d4a1c
zhu-to-u <2528487925@qq.com>
2023-04-03 13:49:07 +0800

    一个简单的c++程序

--- C++_Primer/prog1.cc
+++ C++_Primer/prog1.cc
@@ -6,7 +6,7 @@ int main()
     std::cout << "Enter two numbers:" << std::endl;
     int v1=0,v2=0;
     std::cin >> v1 >> v2;
-    std::cout << "The sum of" << v1 << "and" << v2 << "is" << v1+v2<<std::endl;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1+v2<<std::endl;
 
     return 0;
 
