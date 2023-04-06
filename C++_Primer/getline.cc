99b10c0b9b0630f6362fc5d3155fb03b6a4e5b9c
zhu-to-u <2528487925@qq.com>
2023-04-06 20:52:16 +0800

    getline

--- C++_Primer/getline.cc
+++ C++_Primer/getline.cc
@@ -7,7 +7,7 @@ using std::string;
 int main()
 {
     string line;
-    while(getline(cin,line)){
+    while(getline(cin,line)){//getline返回的换行符被丢弃了,得到的string对象中不含换行符
         cout<<line<<endl;
     }
 
