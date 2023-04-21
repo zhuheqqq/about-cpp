f3d1e594395c1f2a90460276ce1a222ac2668a0b
zhu-to-u <2528487925@qq.com>
2023-04-21 19:49:44 +0800

    链表删除

--- bilibili/构造函数.cpp
+++ bilibili/构造函数.cpp
@@ -12,7 +12,8 @@ private:
 public:
     Person()
     {
-        cout<<"Person的默认构造函数调用"<<endl;
+        this->age=0;
+        cout<<"Person的默认构造函数调用,p1的年龄为"<<age<<endl;
     }
 
     Person(int a)
@@ -32,16 +33,28 @@ public:
         return age;
     }
 
+    //析构函数
+    ~Person()//释放开在堆区的变量
+    {
+
+    }
+
 
 
 };
 
 int main()
 {
-    Person p(18);
-    Person p2(p);
+    Person p;//不要用括号法调用无参构造函数,会被编译器认为是函数的声明
+    Person p1(18);
+    Person p2(p);//拷贝构造
+    Person p3=Person(10);//显示法
+    Person(10);//匿名对象执行完立即释放
+
+    //不要用拷贝构造函数初始化匿名对象
 
     cout<<"p2的年龄为"<<p2.getAge()<<endl;
+    cout<<"p3的年龄为"<<p3.getAge()<<endl;
 
     // system("pause");
 
