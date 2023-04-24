e6f3bc07c1bfafbb569c4470c1fb6902eba794b5
zhu-to-u <2528487925@qq.com>
2023-04-24 19:53:45 +0800

    单例模式打印机

--- bilibili/单例模式打印机.cpp
+++ bilibili/单例模式打印机.cpp
@@ -8,9 +8,18 @@ public:
     {
         return printer;//返回指针,提供只读接口
     }
+    
+    void printerText(string text)
+    {
+        cout<<text<<endl;
+        cnt++;
+    }
+    
+    int cnt;//打印次数
+    
 private:
     Printer(){
-        cout<<"打印机构造函数"<<endl;
+        //cout<<"打印机构造函数"<<endl;
     };//构造函数私有化
     
     Printer(const Printer &p){
@@ -25,6 +34,12 @@ Printer *Printer::printer=new Printer;
 void test01()
 {
     Printer *p1=Printer::getInstance();
+    p1->printerText("summer");
+    cout<<"打印机打印次数:"<<p1->cnt<<endl;
+    
+    Printer *p2=Printer::getInstance();
+    p2->printerText("u.");
+    cout<<"打印机打印次数:"<<p1->cnt<<endl;
 }
 
 int main()
